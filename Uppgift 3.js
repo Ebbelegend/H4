1882034e0f7b4b4184b3b382af5ce908d7ea1f6c
Source Academy User <No public email provided>
2022-11-29 19:36:06 +0100

    Ny uppgift 3

--- Uppgift 3.js
+++ Uppgift 3.js
@@ -1,43 +1,34 @@
+
 function insert(bst, item) {
     const nothing = "";
     return is_null(bst)
-           ? make_tree(item, nothing, nothing)
+           ? make_tree(item, null, null)
            : item === entry(bst)
            ? bst
            : item < entry(bst)
            ? make_tree(entry(bst),
-                       insert(item, left_branch(bst)),
+                       insert(left_branch(bst), item),
                        right_branch(bst))
            : 
-             bst;
+             make_tree(entry(bst),
+                       left_branch(bst),
+                       insert(right_branch(bst), item));
 }
 
 // Copy your binary_search_tree_to_string function here from Task 1.
 function binary_search_tree_to_string(bst) {
-    const empty = "; ";
+    const semicolon = "; ";
     const nothing = "";
     function is_element_true(x, set) {
     return is_null(set) 
-           ? false
-           : x === entry(set) 
-           ? true
-           : x < entry(set)
-           ? is_element_true(x, left_branch(set))
-           :is_element_true(x, right_branch(set));
+           ? x
+           : is_element_true(entry(set), left_branch(set)) + semicolon + is_element_true(x, right_branch(set));
     }
-    function addelement(x, bst) {
-        return is_element_true(x, bst)
-        ? x + empty
-        : nothing;
-    }
-    return addelement(entry(left_branch(left_branch(bst))), bst) +
-    addelement(entry(left_branch(bst)), bst) + addelement(entry(bst), bst) +
-    addelement(entry(left_branch(right_branch(bst))), bst) +
-    addelement(entry(right_branch(bst)), bst);
+    return is_element_true(nothing, bst);
 }
 // Test
 
-binary_search_tree_to_string(insert(make_empty_tree(), "x"));
+//binary_search_tree_to_string(insert(make_empty_tree(), "x"));
 // Should produce "x; "
 
 //const bst = accumulate((item, bst) => insert(bst, item),
@@ -49,5 +40,3 @@ binary_search_tree_to_string(insert(make_empty_tree(), "x"));
 //const pkd_names_with_aaaaron =  insert(pkd_names, "AAAARON NORAAAA");
 //binary_search_tree_to_string(pkd_names_with_aaaaron);
 // Should produce "AAAARON NORAAAA; ..."
-
-
