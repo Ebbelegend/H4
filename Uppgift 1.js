83047b4955d92685bcfa56271714ad6dd78883a1
Source Academy User <No public email provided>
2022-11-29 19:37:08 +0100

    Nyaste

--- Uppgift 1.js
+++ Uppgift 1.js
@@ -1,21 +1,3 @@
-
-function insert(bst, item) {
-    const nothing = "";
-    return is_null(bst)
-           ? make_tree(item, null, null)
-           : item === entry(bst)
-           ? bst
-           : item < entry(bst)
-           ? make_tree(entry(bst),
-                       insert(left_branch(bst), item),
-                       right_branch(bst))
-           : 
-             make_tree(entry(bst),
-                       left_branch(bst),
-                       insert(right_branch(bst), item));
-}
-
-// Copy your binary_search_tree_to_string function here from Task 1.
 function binary_search_tree_to_string(bst) {
     const semicolon = "; ";
     const nothing = "";
@@ -26,17 +8,14 @@ function binary_search_tree_to_string(bst) {
     }
     return is_element_true(nothing, bst);
 }
+    
+    
+const h = make_tree("h", make_empty_tree(), make_empty_tree());
+const a = make_tree("a", make_empty_tree(), make_empty_tree());
+const n = make_tree("n", h, make_empty_tree());
+const c = make_tree("c", a, make_empty_tree());
+const test_bst = make_tree("e", c, n);
+
 // Test
-
-//binary_search_tree_to_string(insert(make_empty_tree(), "x"));
-// Should produce "x; "
-
-//const bst = accumulate((item, bst) => insert(bst, item),
-                       //make_empty_tree(),
-                       //list("g", "a", "r", "x", "p"));
-//binary_search_tree_to_string(bst);
-// Should produce "a; g; p; r; x; "
-
-//const pkd_names_with_aaaaron =  insert(pkd_names, "AAAARON NORAAAA");
-//binary_search_tree_to_string(pkd_names_with_aaaaron);
-// Should produce "AAAARON NORAAAA; ..."
+binary_search_tree_to_string(test_bst);
+binary_search_tree_to_string(pkd_names);
